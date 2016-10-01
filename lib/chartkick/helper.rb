90d1457f8fc5c6e14315d475b1d91f42b0bf46d7
Kirk Elifson <elifsonk@gmail.com>
2016-10-01 03:47:56 -0400

    Added support for script-src CSP (#293)
    * Added support for script-src CSP
    
    * Aligned script-src CSP nonce option with code style. Now using html_escape
    
    * return nil if no nonce
    
    * parens
    
    * oops

--- lib/chartkick/helper.rb
+++ lib/chartkick/helper.rb
@@ -46,13 +46,13 @@ module Chartkick
       defer = !!options.delete(:defer)
       # content_for: nil must override default
       content_for = options.key?(:content_for) ? options.delete(:content_for) : Chartkick.content_for
-
+      nonce = options.key?(:nonce) ? ((%( nonce="%{nonce_value}")) % {nonce_value: ERB::Util.html_escape(options.delete(:nonce))}) : nil
       html = (options.delete(:html) || %(<div id="%{id}" style="height: %{height}; width: %{width}; text-align: center; color: #999; line-height: %{height}; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Loading...</div>)) % {id: ERB::Util.html_escape(element_id), height: ERB::Util.html_escape(height), width: ERB::Util.html_escape(width)}
 
       createjs = "new Chartkick.#{klass}(#{element_id.to_json}, #{data_source.respond_to?(:chart_json) ? data_source.chart_json : data_source.to_json}, #{options.to_json});"
       if defer
         js = <<JS
-<script type="text/javascript">
+<script type="text/javascript"#{nonce}>
   (function() {
     var createChart = function() { #{createjs} };
     if (window.addEventListener) {
@@ -67,7 +67,7 @@ module Chartkick
 JS
       else
         js = <<JS
-<script type="text/javascript">
+<script type="text/javascript"#{nonce}>
   #{createjs}
 </script>
 JS
