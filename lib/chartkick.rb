0f30fe26b6632e5afb710f687829726978bf4207
Andrew Kane <andrew@chartkick.com>
2020-09-26 00:27:36 -0700

    Moved enumerable code to separate file

--- lib/chartkick.rb
+++ lib/chartkick.rb
@@ -1,3 +1,5 @@
+# modules
+require "chartkick/enumerable"
 require "chartkick/helper"
 require "chartkick/version"
 
@@ -17,30 +19,4 @@ module Chartkick
     attr_accessor :options
   end
   self.options = {}
-end
-
-# for both multiple series and
-# making sure hash order is preserved in JavaScript
-module Enumerable
-  def chart_json
-    if is_a?(Hash)
-      if (key = keys.first) && key.is_a?(Array) && key.size == 2
-        group_by { |k, _v| k[0] }.map do |name, data|
-          {name: name, data: data.map { |k, v| [k[1], v] }}
-        end
-      else
-        to_a
-      end
-    elsif is_a?(Array)
-      map do |v|
-        if v.is_a?(Hash) && v[:data].is_a?(Hash)
-          v = v.dup
-          v[:data] = v[:data].to_a
-        end
-        v
-      end
-    else
-      self
-    end.to_json
-  end
 end
