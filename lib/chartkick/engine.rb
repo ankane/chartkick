1257f1a047d5e1610bd6b34f65cdf90a8396b44e
Dieter Lunn <coder2000@gmail.com>
2021-11-10 16:44:00 -0600

    fix: remove importmap check

--- lib/chartkick/engine.rb
+++ lib/chartkick/engine.rb
@@ -3,11 +3,9 @@ module Chartkick
     # for assets
 
     # for importmap
-    if defined?(Importmap)
-      initializer "chartkick.importmap", after: "importmap" do |app|
-        app.config.assets.precompile << "chartkick.js"
-        app.config.assets.precompile << "Chart.bundle.js"
-      end
+    initializer "chartkick.importmap", after: "importmap" do |app|
+      app.config.assets.precompile << "chartkick.js"
+      app.config.assets.precompile << "Chart.bundle.js"
     end
   end
 end
