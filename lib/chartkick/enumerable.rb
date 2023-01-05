959b580f0e016df37495ce850d75e4c4ee8c239e
Andrew Kane <andrew@ankane.org>
2023-01-04 17:59:12 -0800

    Define chart_json on Array and Hash instead of Enumerable - fixes #576

--- lib/chartkick/enumerable.rb
+++ lib/chartkick/enumerable.rb
@@ -1,25 +1,26 @@
 # for both multiple series and
 # making sure hash order is preserved in JavaScript
-module Enumerable
+
+class Array
   def chart_json
-    if is_a?(Hash)
-      if (key = keys.first) && key.is_a?(Array) && key.size == 2
-        group_by { |k, _v| k[0] }.map do |name, data|
-          {name: name, data: data.map { |k, v| [k[1], v] }}
-        end
-      else
-        to_a
+    map do |v|
+      if v.is_a?(Hash) && v[:data].is_a?(Hash)
+        v = v.dup
+        v[:data] = v[:data].to_a
       end
-    elsif is_a?(Array)
-      map do |v|
-        if v.is_a?(Hash) && v[:data].is_a?(Hash)
-          v = v.dup
-          v[:data] = v[:data].to_a
-        end
-        v
+      v
+    end.to_json
+  end
+end
+
+class Hash
+  def chart_json
+    if (key = keys.first) && key.is_a?(Array) && key.size == 2
+      group_by { |k, _v| k[0] }.map do |name, data|
+        {name: name, data: data.map { |k, v| [k[1], v] }}
       end
     else
-      self
+      to_a
     end.to_json
   end
 end
